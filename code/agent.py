485ffb5ea3bd2ce7c20371f90da9e251ed8d6b11
John Patterson Miller <millerjp@stanford.edu>
2015-07-22 11:47:57 -0700

    Implemented basic td_learner

--- code/agent.py
+++ code/agent.py
@@ -1,3 +1,4 @@
+import random
 import numpy as np
 
 
@@ -19,7 +20,7 @@ class ValueIterationSolver(OnlineAgent):
         self.tol = tol
 
         # Tabular representation of state-value function initialized uniformly
-        self.V = [(1. / self.num_states) for s in xrange(self.num_states)]
+        self.V = [1. for s in xrange(self.num_states)]
 
     def get_action(self, state):
         '''Returns the greedy action with respect to the current policy'''
@@ -38,6 +39,9 @@ class ValueIterationSolver(OnlineAgent):
             if val > best_val:
                 best_action = action
                 best_val = val
+            elif val == best_val and random.random() < 0.5:
+                best_action = action
+                best_val = val
 
         return best_action
 
@@ -47,8 +51,12 @@ class ValueIterationSolver(OnlineAgent):
             # repeatedly perform the Bellman backup on each state
             # V_{i+1}(s) = \max_{a} \sum_{s' \in NS} T(s, a, s')[R(s, a, s') + \gamma V(s')]
             max_diff = 0.
+
+            # TODO: Add priority sweeping
             for state in xrange(self.num_states):
                 poss_actions = self.mdp.get_allowed_actions(state)
+
+                # terminal state
                 if len(poss_actions) == 0:
                     self.V[state] = 0.
 
@@ -74,25 +82,86 @@ class ValueIterationSolver(OnlineAgent):
 
 
 class TdLearner(OnlineAgent):
-    ''' Tabular TD-learning (Q-learning, SARSA, etc.) '''
+    ''' Tabular TD-learning (Q-learning, SARSA, etc.)
 
-    def __init__(self, options, environment):
-        pass
+        TODO: Add prioritized sweeping, planning, eligibility traces
+    '''
+
+    def __init__(self, task, update='q_learning', epsilon=0.05, alpha=0.1):
+        # task related set-up
+        self.task = task
+        self.num_states = task.get_num_states()
+        self.num_actions = task.get_num_actions()
+        self.gamma = task.gamma
+
+        # exploration policy
+        self.epsilon = epsilon
+
+        # learning rate
+        self.alpha = alpha
+
+        # Q-learning or SARSA
+        if update not in ['q_learning', 'sarsa']:
+            raise NotImplementedError()
+        self.update = update
+
+        # Tabular representation of the Q-function initialized uniformly
+        self.Q = [[1 for a in xrange(task.get_allowed_actions(s))] for s in xrange(self.num_states)]
+
+        # used for streaming updates
+        self.s0 = None
+        self.a0 = None
+        self.r = None
+        self.s1 = None
+        self.a1 = None
+
+    def _greedy_action(self, state):
+        ''' a^* = argmax_{a} Q(s, a) '''
+        q_vals = self.Q[state]
+        return q_vals.index(max(q_vals))
 
     def get_action(self, state):
+        poss_actions = self.task.get_allowed_actions()
+
         # e-greedy exploration policy
-        # use the estimate of Q-learning
-        pass
+        if random.random() < self.epsilon:
+            action = random.choice(poss_actions)
+        else:
+            action = self._greedy_action(state)
+
+        self.s0 = self.s1
+        self.a0 = self.a1
+        self.s1 = state
+        self.a1 = action
+
+        return action
 
     def learn(self, reward):
-        # Q-learning updates?
-        pass
+        if(self.s0 is not None):
+            if self.update == 'q_learning':
+                next_action = self._greedy_action(self.s1)
+            elif self.update == 'sarsa':
+                # on-policy
+                next_action = self.a1
+            else:
+                raise NotImplementedError()
+
+            self._td_update(self.s0, self.a0, self.r, self.s1, next_action)
+
+            # TODO: add planning here (sample experience from estimated model)
+
+        self.r = reward
+
+    def _td_update(self, s0, a0, r, s1, a1):
+        #Q(s0, a0) + \alpha(r + \gamma Q(s1, a1) - Q(s0, a0))
+        td_error = r + self.gamma * self.Q[s1][a1] - self.Q[s0][a0]
+        self.Q[s0][a0] = self.Q[s0][a0] + self.alpha * td_error
 
 
 class DQN(OnlineAgent):
     ''' Q-learning with a neural network function approximator '''
 
-    def __init__(self, options, environment):
+    def __init__(self, options, task):
         pass
 
     def get_action(self, state):
@@ -105,7 +174,7 @@ class DQN(OnlineAgent):
 class ReinforceAgent(OnlineAgent):
     ''' Policy Gradient with a neural network function approximator '''
 
-    def __init__(self, options, environment):
+    def __init__(self, options, task):
         pass
 
     def get_action(self, state):
