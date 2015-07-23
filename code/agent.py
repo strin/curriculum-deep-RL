8df2c7c7f0dd4b02565ad90f7d30ae8896d362ff
John Patterson Miller <millerjp@stanford.edu>
2015-07-23 15:49:54 -0700

    Removed TDAgent's streaming updates to handle episodic tasks, simplified code

--- code/agent.py
+++ code/agent.py
@@ -9,7 +9,7 @@ class OnlineAgent(object):
     def get_action(self, state):
         raise NotImplementedError()
 
-    def learn(self, reward):
+    def learn(self, next_state, reward):
         raise NotImplementedError()
 
 
@@ -49,10 +49,7 @@ class ValueIterationSolver(OnlineAgent):
 
     def learn(self):
         ''' Performs value iteration on the MDP until convergence '''
-        iter_num = 0
         while True:
-            print 'iteration', iter_num
-            iter_num += 1
             # repeatedly perform the Bellman backup on each state
             # V_{i+1}(s) = \max_{a} \sum_{s' \in NS} T(s, a, s')[R(s, a, s') + \gamma V(s')]
             max_diff = 0.
@@ -61,20 +58,15 @@ class ValueIterationSolver(OnlineAgent):
             for state in xrange(self.num_states):
                 poss_actions = self.mdp.get_allowed_actions(state)
 
-                # terminal state
-                if len(poss_actions) == 0:
-                    self.V[state] = 0.
-                    continue
-
-                best_val = -float('inf')
-                for action in poss_actions:
+                best_val = 0
+                for idx, action in enumerate(poss_actions):
                     val = 0.
                     ns_dist = self.mdp.next_state_distribution(state, action)
                     for ns, prob in ns_dist:
                         val += prob * (self.mdp.get_reward(state, action, ns) +
                                        self.gamma * self.V[ns])
 
-                    if val > best_val:
+                    if(idx == 0 or val > best_val):
                         best_val = val
 
                 diff = abs(self.V[state] - best_val)
@@ -87,10 +79,15 @@ class ValueIterationSolver(OnlineAgent):
                 break
 
 
-class TdLearner(OnlineAgent):
+class TDLearner(OnlineAgent):
     ''' Tabular TD-learning (Q-learning, SARSA, etc.)
 
         TODO: Add prioritized sweeping, planning, eligibility traces
+              To add planning:
+                1) Store a model of experiences (either store samples or an
+                   ML estimate of the dynamics + rewards)
+                2) On each update in learn, sample up to N experiences and
+                   use them to perform updates
     '''
 
     def __init__(self, task, update='q_learning', epsilon=0.05, alpha=0.1):
@@ -112,56 +109,60 @@ class TdLearner(OnlineAgent):
         self.update = update
 
         # Tabular representation of the Q-function initialized uniformly
-        self.Q = [[1 for a in xrange(task.get_allowed_actions(s))] for s in xrange(self.num_states)]
+        self.Q = [[1. for a in task.get_allowed_actions(s)] for s in xrange(self.num_states)]
 
         # used for streaming updates
-        self.s0 = None
-        self.a0 = None
-        self.r = None
-        self.s1 = None
-        self.a1 = None
+        self.last_state = None
+        self.last_action = None
 
-    def _greedy_action(self, state):
-        ''' a^* = argmax_{a} Q(s, a) '''
+    def reset_episode(self):
+        self.last_state = None
+        self.last_action = None
+
+    def _sample_from_policy(self, state):
+        '''epsilon-greedy exploration policy '''
+        poss_actions = self.task.get_allowed_actions(state)
+
+        if len(poss_actions) == 0:
+            return None
+
+        if random.random() < self.epsilon:
+            return random.choice(poss_actions)
+
+        # a^* = argmax_{a} Q(s, a)
         q_vals = self.Q[state]
         return q_vals.index(max(q_vals))
 
     def get_action(self, state):
-        poss_actions = self.task.get_allowed_actions()
-
-        # e-greedy exploration policy
-        if random.random() < self.epsilon:
-            action = random.choice(poss_actions)
-        else:
-            action = self._greedy_action(state)
-
-        self.s0 = self.s1
-        self.a0 = self.a1
-        self.s1 = state
-        self.a1 = action
-
+        action = self._sample_from_policy(state)
+        self.last_state = state
+        self.last_action = action
         return action
 
-    def learn(self, reward):
-        if(self.r is not None):
-            if self.update == 'q_learning':
-                next_action = self._greedy_action(self.s1)
-            elif self.update == 'sarsa':
-                # on-policy
-                next_action = self.a1
+    def _td_update(self, s0, a0, r, s1):
+        if self.update == 'q_learning':
+            # Q(s0, a0) + \alpha(r + \gamma \max_{a} Q(s1, a) - Q(s0, a0))
+            qmax = 0.
+            for idx, qval in enumerate(self.Q[s1]):
+                if(idx == 0 or qval > qmax):
+                    qmax = qval
+            td_error = r + self.gamma * qmax - self.Q[s0][a0]
+        elif self.update == 'sarsa':
+            # Q(s0, a0) + \alpha(r + \gamma Q(s1, a1) - Q(s0, a0))
+            a1 = self._sample_from_policy(s1)
+
+            if a1 is None:  # handle terminal states
+                td_error = r - self.Q[s0][a0]
             else:
-                raise NotImplementedError()
+                td_error = r + self.gamma * self.Q[s1][a1] - self.Q[s0][a0]
+        else:
+            raise NotImplementedError()
+        self.Q[s0][a0] += self.alpha * td_error
 
-            self._td_update(self.s0, self.a0, self.r, self.s1, next_action)
+    def learn(self, next_state, reward):
+        self._td_update(self.last_state, self.last_action, reward, next_state)
 
-            # TODO: add planning here (sample experience from estimated model)
-
-        self.r = reward
-
-    def _td_update(self, s0, a0, r, s1, a1):
-        #Q(s0, a0) + \alpha(r + \gamma Q(s1, a1) - Q(s0, a0))
-        td_error = r + self.gamma * self.Q[s1][a1] - self.Q[s0][a0]
-        self.Q[s0][a0] = self.Q[s0][a0] + self.alpha * td_error
+        # TODO: Add planning HERE;
 
 
 class DQN(OnlineAgent):
