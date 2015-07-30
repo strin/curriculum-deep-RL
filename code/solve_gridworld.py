bc696847c901018c8019e24d492f17630a7cfe4e
jpmpentwater <millerjp@stanford.edu>
2015-07-29 19:04:27 -0700

    Solve simple grid world with DQN

--- code/solve_gridworld.py
+++ code/solve_gridworld.py
@@ -1,5 +1,5 @@
 from gridworld import Grid, GridWorldMDP, GridWorld
-from agent import ValueIterationSolver, TDLearner
+from agent import ValueIterationSolver, TDLearner, DQN
 import numpy as np
 
 # simple world with a single wall
@@ -61,27 +61,36 @@ NUM_EPISODES = 50000
 grid_task = GridWorld(grid, rewards, wall_penalty=0., gamma=0.9)
 td_agent = TDLearner(grid_task, update='sarsa', alpha=0.05, epsilon=0.05)
 for i in xrange(NUM_EPISODES):
+print "Solving with DQN"
+NUM_EPISODES = 100000
+grid = Grid(world, action_stoch=0.2)
+grid_task = GridWorld(grid, rewards, wall_penalty=0., gamma=0.9, tabular=False)
+dqn = DQN(grid_task, hidden_dim=50, l2_reg=0.0, lr=1e-2, epsilon=0.1)
+for episode in xrange(NUM_EPISODES):
     curr_state = grid_task.get_current_state()
     while(not grid_task.is_terminal()):
-        action = td_agent.get_action(curr_state)
+        action = dqn.get_action(curr_state)
         next_state, reward = grid_task.perform_action(action)
-        td_agent.learn(next_state, reward)
+        dqn.learn(next_state, reward)
         curr_state = next_state
 
     grid_task.reset()
-    td_agent.reset_episode()
+    dqn.reset_episode()
 
+    if episode % 1000 == 0:
+        values = np.zeros(world.shape)
+        for row in xrange(world.shape[0]):
+            for col in xrange(world.shape[1]):
+                if world[row, col] == 0:  # agent can occupy this state
+                    agent_state = np.zeros_like(world)
+                    agent_state[row, col] = 1.
+                    state = np.concatenate((agent_state.ravel(), world.ravel())).reshape(-1, 1)
 
-values = np.zeros(world.shape)
-for state in xrange(grid.get_num_states()):
-    qvals = td_agent.Q[state]
-    if len(qvals) == 0:
-        val = 0
-    else:
-        val = max(qvals)
-    values[grid.state_pos[state]] = val
+                    qvals = dqn.fprop(state)
+                    values[row, col] = np.max(qvals)
 
-for pos, r in rewards.items():
-    values[pos] = r
+        for pos, r in rewards.items():
+            values[pos] = r
 
-print values
+        print "\n"
+        print values
