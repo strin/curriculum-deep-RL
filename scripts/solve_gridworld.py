42e6c7a4c6ec3cd8bf5ab9edbde6bf7cbd48b5fd
jpmpentwater <millerjp@stanford.edu>
2015-08-04 18:09:46 -0700

    Updated recurrent reinforce plotting code

--- scripts/solve_gridworld.py
+++ scripts/solve_gridworld.py
@@ -24,7 +24,7 @@ rewards = {(0, 3): 1., (1, 3): -1.}
 grid = Grid(world, action_stoch=0.2)
 
 
-# In[7]:
+# In[3]:
 
 # Solve the grid using value iteration as a sanity check
 mdp = GridWorldMDP(grid, rewards, wall_penalty=0., gamma=0.9)
@@ -36,13 +36,17 @@ values = np.zeros(world.shape)
 for state in xrange(grid.get_num_states()):
     values[grid.state_pos[state]] = mdp_agent.V[state]
 
+expected_reward = np.mean(values)
+
+# put the terminal state rewards in for visualization purposes
 for pos, r in rewards.items():
     values[pos] = r
 
 print values
+print 'Expected reward', expected_reward
 
 
-# In[37]:
+# In[4]:
 
 # template for interaction between the agent and the task
 def grid_experiment(agent, task, num_episodes, diagnostic_callback=None, diagnostic_frequency=100):
@@ -100,7 +104,7 @@ def compute_value_function(episode, total_reward):
 grid_experiment(dqn, grid_task, NUM_EPISODES, diagnostic_callback=compute_value_function, diagnostic_frequency=1000)
 
 
-# In[73]:
+# In[ ]:
 
 # Solve gridworld using the recurrent reinforce agent (policy gradient)
 
@@ -113,19 +117,30 @@ rr_agent = RecurrentReinforceAgent(grid_task, hidden_dim=1024)
 # define diagnostic function (maintains internal state)
 class plot_historical_avg():
     def __init__(self):
-        self.running_avg_reward = 0.
-        self.avg_reward_hist = []
+#         self.running_avg_reward = 0.
+#         self.avg_reward_hist = []
+        self.reward_hist = []
+        self.moving_avg_reward = []
+        self.episodes = []
     
     def __call__(self, episode, total_reward):
-        self.running_avg_reward += (1. / (episode + 1)) * (total_reward - self.running_avg_reward)
-        self.avg_reward_hist.append(self.running_avg_reward)
-            self.reward_history.append(total_reward)
-            if episode % 20 == 0:
-                plt.plot(np.arange(episode + 1), self.avg_reward_hist)
-                plt.xlabel('Iterations')
-                plt.ylabel('Average Reward')
-                display.display(plt.gcf())
-                display.clear_output(wait=True)
+#         self.running_avg_reward += (1. / (episode + 1)) * (total_reward - self.running_avg_reward)
+#         self.avg_reward_hist.append(self.running_avg_reward)
+        self.reward_hist.append(total_reward)
+        if episode % 25 == 0:
+            self.moving_avg_reward.append(np.mean(self.reward_hist))
+            # clear history
+            self.reward_hist = []
+            self.episodes.append(episode)
+           
+            # plotting
+            plt.plot(self.episodes, expected_reward * )
+            plt.plot(self.episodes, self.moving_avg_reward, 'b')
+            plt.xlabel('Iterations')
+            plt.ylabel('Average Reward')
+            plt.title('Average Reward over The Last 50 time steps versus Iteration')
+            display.display(plt.gcf())
+            display.clear_output(wait=True)
 
     
 historical_averager = plot_historical_avg()
