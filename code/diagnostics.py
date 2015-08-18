c8b67f2583a61f198d4f9b38f3eb54759a04820b
jpmpentwater <millerjp@stanford.edu>
2015-08-18 11:45:23 -0700

    Observer to visualize trajectories during training

--- code/diagnostics.py
+++ code/diagnostics.py
@@ -1,8 +1,11 @@
+import os
+import cPickle as pickle
 import pylab as plt
 import util
+from experiment import Controller
 
 
-def visualize_sample_trajectory(agent, task):
+def visualize_sample_trajectory(agent, task, path=None):
     '''
         Useful to visualize 2D Trajectories inside IPython
     '''
@@ -12,17 +15,10 @@ def visualize_sample_trajectory(agent, task):
         print 'Must be inside IPython!'
         raise NotImplementedError()
 
-    # sample a trajectory
-    task.reset()
-    current_state = task.get_start_state()
-    images = [task.visualize()]
-    while True:
-        action = agent.get_action(current_state)
-        next_state, reward = task.perform_action(action)
-        images.append(task.visualize())
-        if task.is_terminal():
-            break
-        current_state = next_state
+    if path is None:
+        images = get_sample_trajectory(agent, task)
+    else:
+        images = pickle.load(file(path, 'r'))
 
     # show the position of the agent across time
     def show_image(image):
@@ -32,3 +28,40 @@ def visualize_sample_trajectory(agent, task):
 
     for image in images:
         show_image(image)
+
+
+def get_sample_trajectory(agent, task):
+    '''
+        Returns a list of images (matrices) based on a single sampled
+        trajectory of the agent.
+    '''
+    # sample a trajectory
+    task.reset()
+    current_state = task.get_start_state()
+    images = [task.visualize()]
+    while True:
+        action = agent.get_action(current_state)
+        next_state, reward = task.perform_action(action)
+        images.append(task.visualize())
+        if task.is_terminal():
+            if hasattr(agent, 'end_episode'):  # for recurrent agents.
+                agent.end_episode(0, no_learning=True)
+            break
+        current_state = next_state
+    return images
+
+
+class VisualizeTrajectoryController(Controller):
+    def __init__(self, visualize_wait=100, dir_name='trajectories'):
+        self.dir_name = dir_name
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+        self.visualize_wait = visualize_wait
+
+    def control(self, experiment):
+        if experiment.num_episodes % self.visualize_wait == 0:
+            trajectory = get_sample_trajectory()
+            print 'Saving trajectory...'
+            file_name = 'trajectory_' + str(experiment.num_episodes) + '.cpkl'
+            pickle.dump(trajectory, file(os.path.join(self.dir_name, file_name), 'wb'),
+                        protocol=pickle.HIGHEST_PROTOCOL)
