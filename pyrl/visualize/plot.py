85785514823105cdf84db669da364c1cd997a336
strin <stl501@gmail.com>
2016-05-01 17:24:44 -0700

    add continuous error plot

--- pyrl/visualize/plot.py
+++ pyrl/visualize/plot.py
@@ -1,4 +1,5 @@
 # plotly utils.
+from pyrl.common import *
 import plotly
 import plotly.graph_objs as go
 from plotly.offline import download_plotlyjs, init_notebook_mode, iplot
@@ -61,6 +62,80 @@ def plot_xy(x, y, names=None, error_ys=None, xlabel='x', ylabel='y', title=''):
         traces.append(trace)
 
     data = traces
+    fig = go.Figure(data=data, layout=layout)
+    disp = iplot(fig)
+
+
+def plot_xye(x, y, err, names=None, xlabel='x', ylabel='y', title=''):
+
+    layout = go.Layout(
+        title=title,
+        xaxis=dict(
+            title=xlabel,
+            titlefont=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'
+            )
+        ),
+        yaxis=dict(
+            title=ylabel,
+            titlefont=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'
+            )
+        )
+    )
+
+    if type(x) == list:
+        xs = x
+        ys = y
+        if not names:
+            names = ['line-' + str(i) for i in range(len(xs))]
+    else:
+        xs = [x]
+        ys = [y]
+        names = ['line']
+
+    traces = []
+    traces_std = []
+
+    std_colors = ['rgba(44,160,44,0.2)',
+                    'rgba(214,39,40,0.2)',
+                  ]
+
+    for (i, (x, y, e)) in enumerate(zip(xs, ys, err)):
+        kwargs = {}
+        if names:
+            kwargs['name'] = names[i]
+        y = np.array(y)
+        e = np.array(e)
+        y_upper = list(y + e)
+        y_lower = list(y - e)
+        x = list(x)
+        print 'y-upper', y_upper
+        print 'y-lower', y_lower
+        print 'y', y
+        trace_std = go.Scatter(
+            x = x + x[::-1],
+            y = y_upper + y_lower[::-1],
+            fill='tozerox',
+            fillcolor=std_colors[i],
+            line=go.Line(color='transparent'),
+            showlegend=False,
+            **kwargs
+        )
+        traces_std.append(trace_std)
+
+        trace = go.Scatter(
+            x = x,
+            y = y,
+            **kwargs
+        )
+        traces.append(trace)
+
+    data = traces_std + traces
     fig = go.Figure(data=data, layout=layout)
     disp = iplot(fig)
 
