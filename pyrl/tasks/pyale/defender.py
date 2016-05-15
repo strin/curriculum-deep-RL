d7b0a4f18787b8fe0b72c77aab49100023172777
strin <stl501@gmail.com>
2016-05-15 00:29:30 -0700

    ram inputs for game defender

--- pyrl/tasks/pyale/defender.py
+++ pyrl/tasks/pyale/defender.py
@@ -3,6 +3,7 @@ from pyrl.common import *
 from pyrl.tasks.pyale import PygameSimulator, function_intercept
 from pyrl.evaluate import DrunkLearner
 from pygame.locals import *
+from pyrl.config import floatX
 import pygame
 
 _name = 'defender'
@@ -34,7 +35,45 @@ class DefenderSimulator(PygameSimulator):
         return score
 
 
+class DefenderRAMSimulator(DefenderSimulator):
+    def __init__(self):
+        DefenderSimulator.__init__(self, state_type='ram')
+
+
+    def _get_ram_state(self):
+        ram = []
+        W = 400.
+        H = 600.
+        ship = self._get_attr('ship')
+        enemi = self._get_attr('enemi')
+        shotami = self._get_attr('shotami')
+        shotenemi = self._get_attr('shotenemi')
+        ram.extend([ship.topleft[0] / W, ship.topleft[1] / H,
+                    (ship.topleft[0] + ship.rect.right - ship.rect.left) / W,
+                    (ship.topleft[1] + ship.rect.top - ship.rect.bottom) / W])
+        C = 10
+        ram_enemi = np.zeros(2 * C) # at most handle 10 enemy ships.
+        for (i, e) in enumerate(enemi[:C]):
+            ram_enemi[i * 2] = e.top / H
+            ram_enemi[i * 2 + 1] = e.left / W
+        ram.extend(list(ram_enemi))
+
+        ram_shotami = np.zeros(2 * C) # at most handle 10 bullets.
+        for (i, e) in enumerate(shotami[:C]):
+            ram_shotami[i * 2] = e.top / H
+            ram_shotami[i * 2 + 1] = e.left / W
+        ram.extend(list(ram_shotami))
+
+        ram_shotenemi = np.zeros(2 * C) # at most handle enemy 10 bullets.
+        for (i, e) in enumerate(shotenemi[:C]):
+            ram_shotenemi[i * 2] = e.top / H
+            ram_shotenemi[i * 2 + 1] = e.left / W
+        ram.extend(list(ram_shotenemi))
+
+        return np.array(ram, dtype=floatX)
+
 
 if __name__ == '__main__':
-    defender = DefenderSimulator(state_type='pixel')
+    # defender = DefenderSimulator(state_type='pixel')
+    defender = DefenderRAMSimulator()
     defender.run(DrunkLearner())
